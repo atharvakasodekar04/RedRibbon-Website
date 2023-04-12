79ad7d1085c04490ecd1a89316b554384195914b
atharvakasodekar04 <97936616+atharvakasodekar04@users.noreply.github.com>
2023-04-12 17:15:41 +0530

    Change in PORT

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 
 const app = express();
-const PORT = "https://redribbon-backend.onrender.com";
+const PORT = "https://redribbon-backend.onrender.com" || 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
